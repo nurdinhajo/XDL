7c0321ce00c6fc4c0fb83bca47971bf6802cfeb3
nurdinhajo <nurdinhajo@gmail.com>
2023-11-01 21:14:55 +0300

    Home Components

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import './home.css'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className="home">
+      <div className="xdl-description">
+        <h1>Fast <span id='middledot'>·</span> Reliable</h1>
+      </div>
+
+      <div className="xdl-youtube">
+
+      </div>
+    </div>
   )
 }
 
