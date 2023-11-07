02cb802d64145d52298f1c6180f3d94290cd9e47
nurdinhajo <nurdinhajo@gmail.com>
2023-11-07 15:38:38 +0300

    Update - Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,9 @@
-import './header.css'
+import { useRef } from "react";
 import { Link } from 'react-router-dom';
+import { FaBars, FaTimes } from "react-icons/fa";
+import "./header.css";
 
-const links = [
-  {
-    name: '',
-    link: '/',
-  },
+const navLinks = [
   {
     name: 'Services',
     link: '/services',
@@ -20,7 +18,7 @@ const links = [
   },
   {
     name: 'Contact',
-    link: '/contact',
+    link: '/contact', 
   },
   {
     name: 'FAQS',
@@ -28,35 +26,54 @@ const links = [
   },
 ];
 
-function Header() {
 
-  return (
-    <header className="header">
+
+function Navbar() {
+	const navRef = useRef();
+
+	const showNavbar = () => {
+		navRef.current.classList.toggle(
+			"responsive_nav"
+		);
+	};
+
+	return (
+		<header>
       <div className="logo">
       <Link to ="/">
         <img src={process.env.PUBLIC_URL + '/XDL-LOGO.png'} alt="XDL Logo" />
         </Link> 
       </div>
-      <nav>
-        {links.map((link) => (
+			<nav ref={navRef}>
+      {navLinks.map((link) => (
           <ul key={link.link}>
             <li>
-              <a href={link.link}>{link.name}</a>
+              <a href={link.link}>{link.name}</a>              
             </li>
+            
           </ul>
         ))}
-        <ul>
-          <li>
-              <a href="http://18.130.30.102/xobo" target='blank'>
-              <button>
-                Transport Login
-              </button>
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
+
+      
+          <a href="http://18.130.30.102/xobo" target='blank'>
+            <button className="orderBtn">
+              Transport Login
+            </button>
+          </a>
+      
+				<button
+					className="nav-btn nav-close-btn"
+					onClick={showNavbar}>
+					<FaTimes />
+				</button>
+			</nav>
+			<button
+				className="nav-btn"
+				onClick={showNavbar}>
+				<FaBars />
+			</button>
+		</header>
+	);
 }
 
-export default Header;
+export default Navbar;
