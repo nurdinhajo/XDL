63b35797b6333f98dfd80d40e25be5aa45b2bb30
nurdinhajo <nurdinhajo@gmail.com>
2023-11-13 08:05:49 +0300

    Update - Header & Home

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -37,7 +37,7 @@ function Header() {
     <header>
       <div className="logo">
         <Link to="/">
-          <img src={process.env.PUBLIC_URL + '/Header-Logo.jpeg'} alt="XOBO Logo" />
+          <img src={process.env.PUBLIC_URL + '/Header-Logo2.jpeg'} alt="XOBO Logo" />
         </Link>
       </div>
 
