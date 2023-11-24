ba9b8ff5f354ff5cfeb40af09e16f2a620c797fd
nurdinhajo <nurdinhajo@gmail.com>
2023-11-24 19:27:59 +0300

    Update - Contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -11,7 +11,11 @@ function Contact() {
     message: '',
   });
 
-  const [emailError, setEmailError] = useState('');
+  const [errors, setErrors] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -19,19 +23,33 @@ function Contact() {
       ...formData,
       [name]: value,
     });
+
+    // Clear the error when user starts typing
+    setErrors({
+      ...errors,
+      [name]: '',
+    });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    // Check if email is empty
-    if (!formData.email) {
-      setEmailError('Please enter your email.');
+    // Check for empty fields and set errors
+    const newErrors = {};
+    Object.keys(formData).forEach((key) => {
+      if (!formData[key]) {
+        newErrors[key] = 'Please complete this required field.';
+      }
+    });
+
+    // If there are errors, set them in the state and prevent form submission
+    if (Object.keys(newErrors).length > 0) {
+      setErrors(newErrors);
       return;
     }
 
-    // Clear email error if email is provided
-    setEmailError('');
+    // Clear errors if there are no issues
+    setErrors({});
 
     // Handle form submission logic here
     console.log('Form submitted:', formData);
@@ -41,9 +59,38 @@ function Contact() {
     <div className="contact">
       <Header />
 
+      <div className="contactDetails">
+        <div className="call">
+         <a href="tel:+254799396000">
+           <img src={process.env.PUBLIC_URL + '/XDL-CALL.png'} alt="XOBO Call" />     
+            +254 799 396 000
+          </a>
+        </div>
+
+        <div className="email">
+         <a href="mailto:info@xobo.co.ke" >
+            <img src={process.env.PUBLIC_URL + '/XDL-EMAIL.png'} alt="XOBO Email" />
+          info@xobo.co.ke
+         </a>
+        </div>
+
+        <div className="whatsapp">
+         <a href="https://api.whatsapp.com/send?phone=254799396000" target='_blank'>
+           <img src={process.env.PUBLIC_URL + '/XDL-WHATSAPP.png'} alt="XOBO WhatsApp" />          
+            +254 799 396 000
+        </a>
+        </div>
+      </div>
+
       <div className="contact-container">
         <form className="contact-form" onSubmit={handleSubmit}>
-          <label htmlFor="name">Name:</label>
+          <h3>
+            Need More Information?
+          </h3>
+          <p>
+            Fill up the form below to send us a message and we will get in touch as soon as possible.
+          </p>
+          <label htmlFor="name">First Name:</label>
           <input
             type="text"
             id="name"
@@ -52,6 +99,18 @@ function Contact() {
             onChange={handleChange}
             required
           />
+          {errors.name && <p className="error-message">{errors.name}</p>}
+
+          <label htmlFor="name">Last Name:</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
+          {errors.name && <p className="error-message">{errors.name}</p>}
 
           <label htmlFor="email">Email:</label>
           <input
@@ -62,7 +121,7 @@ function Contact() {
             onChange={handleChange}
             required
           />
-          {emailError && <p className="error-message">{emailError}</p>}
+          {errors.email && <p className="error-message">{errors.email}</p>}
 
           <label htmlFor="message">Message:</label>
           <textarea
@@ -72,6 +131,7 @@ function Contact() {
             onChange={handleChange}
             required
           ></textarea>
+          {errors.message && <p className="error-message">{errors.message}</p>}
 
           <button type="submit">Submit</button>
         </form>
