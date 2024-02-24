a80af04e48854ae564d4465fdbe1afe82f807475
nurdinhajo <nurdinhajo@gmail.com>
2024-02-24 23:18:40 +0300

    Update - Contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,76 +1,86 @@
-import React, { useState, useEffect, useMemo } from 'react';
-import Header from '../header/Header';
-import Footer from '../footer/Footer';
-import PhoneInput from 'react-phone-input-2';
-import 'react-phone-input-2/lib/style.css';
-import './contact.css';
+import React, { useState } from "react";
+import Header from "../header/Header";
+import Footer from "../footer/Footer";
+import PhoneInput from "react-phone-input-2";
+import "react-phone-input-2/lib/style.css";
+import "./contact.css";
 
 function Contact() {
-  const initialValues = useMemo(() => ({
-    firstName: '',
-    lastName: '',
-    email: '',
-    phoneNumber: '',
-    message: '',
-  }), []); // Empty dependency array since it doesn't depend on any variables
-
-  const [formData, setFormData] = useState(initialValues);
+  const [result, setResult] = useState("Send Message");
+  const [phone, setPhone] = useState("");
   const [errors, setErrors] = useState({});
-  const [isSubmit, setIsSubmit] = useState(false);
 
-  const handleChange = (value, name) => {
-    setFormData({ ...formData, [name]: value });
-    setErrors({ ...errors, [name]: '' });
-  };
-
-  const validate = (values) => {
+  const validateForm = (formData) => {
     const errors = {};
-    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+    let formIsValid = true;
 
-    if (!values.firstName) {
-      errors.firstName = 'First Name is required!';
+    // Validate first name
+    if (!formData.get("First Name")) {
+      errors.firstName = "First name is required";
+      formIsValid = false;
     }
 
-    if (!values.lastName) {
-      errors.lastName = 'Last Name is required!';
+    // Validate last name
+    if (!formData.get("Last Name")) {
+      errors.lastName = "Last name is required";
+      formIsValid = false;
     }
 
-    if (!values.phoneNumber) {
-      errors.phoneNumber = 'Phone Number is required!';
-    } else if (values.phoneNumber.length < 10 || values.phoneNumber.length > 12) {
-      errors.phoneNumber = 'Phone Number should be between 10 and 12 digits!';
+    // Validate phone number
+    if (!phone || !phone.match(/^\d{10,12}$/)) {
+      errors.phone = "Phone number should be between 10 and 12 digits!";
+      formIsValid = false;
     }
 
-    if (!values.email) {
-      errors.email = 'Email is required!';
-    } else if (!regex.test(values.email)) {
-      errors.email = 'This is not a valid email format!';
+    // Validate email
+    if (!formData.get("email")) {
+      errors.email = "Email is required";
+      formIsValid = false;
     }
 
-    if (!values.message) {
-      errors.message = 'Message is required!';
+    // Validate message
+    if (!formData.get("message")) {
+      errors.message = "Message is required";
+      formIsValid = false;
     }
 
-    return errors;
+    setErrors(errors);
+    return formIsValid;
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setErrors(validate(formData));
-    setIsSubmit(true);
-  };
+  const onSubmit = async (event) => {
+    event.preventDefault();
 
-  useEffect(() => {
-    const clearForm = () => {
-      setFormData(initialValues);
-      setIsSubmit(false);
-    };
+    const formData = new FormData(event.target);
 
-    if (Object.keys(errors).length === 0 && isSubmit) {
-      console.log('Form submitted:', formData);
-      clearForm();
+    if (validateForm(formData)) {
+      setResult("Sending....");
+      formData.append("access_key", "3b3b723b-5b0c-4168-9679-6acc5dfe3940");
+      formData.append("phone", phone);
+
+      const res = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        body: formData,
+      }).then((res) => res.json());
+
+      if (res.success) {
+        console.log("Success", res);
+        setResult("Message Sent Successfully!");
+
+        // Clear the form and reset the PhoneInput after 3 seconds
+        setTimeout(() => {
+          event.target.reset();
+          setPhone("");
+          setResult("Send Message");
+        }, 3000);
+      } else {
+        console.log("Error", res);
+        setResult(res.message);
+      }
+    } else {
+      setResult("Please fill in all required fields");
     }
-  }, [errors, isSubmit, formData, initialValues]);
+  };
 
   return (
     <div className="contact">
@@ -79,103 +89,120 @@ function Contact() {
       <div className="contactDetails">
         <div className="call">
           <a href="tel:+254799396000">
-            <img src={process.env.PUBLIC_URL + '../media/XDL-CALL.png'} alt="XOBO Call" />
+            <img
+              src={process.env.PUBLIC_URL + "../media/XDL-CALL.png"}
+              alt="XOBO Call"
+            />
             +254 799 396 000
           </a>
         </div>
 
         <div className="email">
           <a href="mailto:info@xobo.co.ke">
-            <img src={process.env.PUBLIC_URL + '../media/XDL-EMAIL.png'} alt="XOBO Email" />
+            <img
+              src={process.env.PUBLIC_URL + "../media/XDL-EMAIL.png"}
+              alt="XOBO Email"
+            />
             info@xobo.co.ke
           </a>
         </div>
 
         <div className="whatsapp">
-          <a href="https://api.whatsapp.com/send?phone=254799396000" target='_blank' rel="noreferrer">
-            <img src={process.env.PUBLIC_URL + '../media/XDL-WHATSAPP.png'} alt="XOBO WhatsApp" />
+          <a
+            href="https://api.whatsapp.com/send?phone=254799396000"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src={process.env.PUBLIC_URL + "../media/XDL-WHATSAPP.png"}
+              alt="XOBO WhatsApp"
+            />
             +254 799 396 000
           </a>
         </div>
       </div>
 
       <div className="contact-container">
-        <form className="contact-form" onSubmit={handleSubmit}>
-          <h3>Need More Information?</h3>
-          <p>Fill up the form below to send us a message, and we will get in touch as soon as possible.</p>
+        <h3>Need More Information?</h3>
+        <p>
+          Fill up the form below to send us a message, and we will get in touch
+          as soon as possible.
+        </p>
+        <form onSubmit={onSubmit}>
+        <input type="hidden" name="subject" value="New Message from XOBO Website" />
 
-          <div className="input-container">
-            <label htmlFor="firstName">First Name:</label>
-            <input
-              type="text"
-              id="firstName"
-              name="firstName"
-              placeholder='Enter Your First names'
-              value={formData.firstName}
-              onChange={(e) => handleChange(e.target.value, 'firstName')}
-              required
-            />
-            {errors.firstName && <p className="error-message">{errors.firstName}</p>}
-          </div>
+          <label htmlFor="firstName">First Name:</label>
+          <input
+            type="text"
+            name="First Name"
+            style={{ borderColor: errors.firstName ? "red" : "" }}
+          />
+          {errors.firstName && (
+            <p style={{ color: "red", marginTop: "-15px", fontWeight: "400" }}>
+              {errors.firstName}
+            </p>
+          )}
 
-          <div className="input-container">
-            <label htmlFor="lastName">Last Name:</label>
-            <input
-              type="text"
-              id="lastName"
-              name="lastName"
-              placeholder='Enter Your Last names'
-              value={formData.lastName}
-              onChange={(e) => handleChange(e.target.value, 'lastName')}
-              required
-            />
-            {errors.lastName && <p className="error-message">{errors.lastName}</p>}
-          </div>
+          <label htmlFor="lastName">Last Name:</label>
+          <input
+            type="text"
+            name="Last Name"
+            style={{ borderColor: errors.lastName ? "red" : "" }}
+          />
+          {errors.lastName && (
+            <p style={{ color: "red", marginTop: "-15px", fontWeight: "400" }}>
+              {errors.lastName}
+            </p>
+          )}
 
-          <div className="input-container">
-            <label htmlFor="phoneNumber">Phone Number:</label>
-            <PhoneInput
-              country={'ke'}
-              id="phoneNumber"
-              onlyCountries={['ke']}
-              value={formData.phoneNumber}
-              placeholder='712 345 678'
-              onChange={(value) => handleChange(value, 'phoneNumber')}
-              inputProps={{
-                required: true,
-              }}
-            />
-            {errors.phoneNumber && <p className="error-message red">{errors.phoneNumber}</p>}
-          </div>
+          <label htmlFor="phoneNumber">Phone Number:</label>
+          <PhoneInput
+            className="phone-input"
+            country={"ke"}
+            id="phoneNumber"
+            onlyCountries={["ke"]}
+            placeholder="+254 712 345 678"
+            value={phone}
+            onChange={setPhone}
+            inputStyle={{
+              borderColor: errors.phone ? "red" : "",
+              border: "none",
+            }}
+            readOnly={true}
+            required
+          />
+          {errors.phone && (
+            <p style={{ color: "red", marginTop: "-15px", fontWeight: "400" }}>
+              {errors.phone}
+            </p>
+          )}
 
-          <div className="input-container">
-            <label htmlFor="email">Email:</label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              placeholder='you@example.com'
-              value={formData.email}
-              onChange={(e) => handleChange(e.target.value, 'email')}
-              required
-            />
-            {errors.email && <p className="error-message red">{errors.email}</p>}
-          </div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            name="email"
+            style={{ borderColor: errors.email ? "red" : "" }}
+          />
+          {errors.email && (
+            <p style={{ color: "red", marginTop: "-15px", fontWeight: "400" }}>
+              {errors.email}
+            </p>
+          )}
 
-          <div className="input-container">
-            <label htmlFor="message">Message:</label>
-            <textarea
-              id="message"
-              name="message"
-              value={formData.message}
-              onChange={(e) => handleChange(e.target.value, 'message')}
-              placeholder='Your Message'
-              required
-            ></textarea>
-            {errors.message && <p className="error-message red">{errors.message}</p>}
-          </div>
+          <label htmlFor="message">Message:</label>
+          <textarea
+            name="message"
+            style={{ borderColor: errors.message ? "red" : "" }}
+          ></textarea>
+          {errors.message && (
+            <p style={{ color: "red", marginTop: "-15px", fontWeight: "400" }}>
+              {errors.message}
+            </p>
+          )}
 
-          <button type="submit">Submit</button>
+          
+
+          <button type="submit">{result}</button>
         </form>
       </div>
 
