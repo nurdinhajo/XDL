70921e869e759ceb0af1f5f2bf0c21b03a738ae2
nurdinhajo <nurdinhajo@gmail.com>
2024-03-27 01:06:21 +0300

    Update - FAQ

--- src/components/faq/FAQPage.jsx
+++ src/components/faq/FAQPage.jsx
@@ -15,7 +15,7 @@ const FAQPage = ({ faqs }) => {
       <a href="#All" onClick={() => handleCategoryChange('All')}>All</a>
       <a href="#Order" onClick={() => handleCategoryChange('order')}>Order</a>
       <a href="#Delivery" onClick={() => handleCategoryChange('delivery')}>Delivery</a>
-      <a href="#Partner" onClick={() => handleCategoryChange('delivery partner')}>Delivery Partner</a>
+      {/* <a href="#Partner" onClick={() => handleCategoryChange('delivery partner')}>Delivery Partner</a> */}
       <a href="#Payments" onClick={() => handleCategoryChange('payments')}>Payments</a>
     </div>
     <div className="questions">
